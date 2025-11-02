8c21dedb32f876410e8f8cdd10c222690b07e399
Emmanuele Levi <ignisvictorious@gmail.com>
2025-11-02 16:12:52 +0000

    Add a TextBield and a Button

--- src/main.py
+++ src/main.py
@@ -8,27 +8,24 @@ import flet as ft
 
 #! The main function def main(page: ft.Page):
 def main(page: ft.Page) -> None:
-    page.title = "30 Days of Flet - 03. Layout: Rows & Columns"
+    page.title = "30 Days of Flet - 04. User Input"
+    page.window.width = 1000  # ! Custom windo
+    page.window.height = 500  # ! Custom windo
 
-    # & Icon control
-    icon = ft.Icon(name=ft.Icons.LINK_OUTLINED, size=24, color="#FFFDD0")
-    icon2 = ft.Icon(name=ft.Icons.EMAIL_OUTLINED, size=24, color="#4C8EFF")
-    icon3 = ft.Icon(name=ft.Icons.PLACE_OUTLINED, size=24, color="#4C8EFF")
+    # Getting user name
+    name = ft.TextField(label="Your name", hint_text="Enter your name...")
 
-    # & Text control
-    text = ft.Text(value="github.com/coderadi-in", size=16, color="#FFFDD0")
-    text2 = ft.Text(value="coder.adi.dev@gmail.com", size=16, color="#FFFFFF")
-    text3 = ft.Text(value="India", size=16, color="#FFFFFF")
+    # * function to handle on click event
+    def handle_on_click(event) -> None:
+        name_value: str | None = name.value
+        if name_value:
+            event.text = f"Hi, {name_value.strip()}"
 
-    link_ui = ft.Row(controls=[icon, text], spacing=10)
-    email_ui = ft.Row(controls=[icon2, text2], spacing=10)
-    place_ui = ft.Row(controls=[icon3, text3], spacing=10)
-
-    # row = ft.Row(controls=[email_ui, place_ui, link_ui], spacing=10)
-    column = ft.Column(controls=[email_ui, place_ui, link_ui], spacing=10)
+    # Creating simple buttons
+    btn = ft.ElevatedButton(text="Click", on_click=handle_on_click)
 
     # page.add(row)
-    page.add(column)
+    page.add(name, btn)
 
 
 # Run the UI
